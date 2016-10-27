08126c3c73db05fb09b679c2c05c32c1bc207103
Спиридонов Илья Александрович <spiridonov-ilya@inbox.ru>
2016-10-27 23:05:35 +0300

    Init

---
+++ kasp2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kasp2
+{
+    class Program
+    {
+        static MySaveQueue<int> q;
+        static void Main(string[] args)
+        {
+            q = new MySaveQueue<int>();
+
+            //Вызываем в произвольном порядке
+            q.Pop();
+            q.Pop();
+            q.Push(3);
+            q.Push(4);
+            q.Pop();
+            q.Pop();
+            q.Push(2);
+            q.Pop();
+            q.Pop();
+            q.Push(1);
+            q.Push(5);
+            q.Push(6);
+
+            Console.ReadLine();
+        }
+
+         
+
+    }
+
+    class MySaveQueue<T>
+    {
+        private Queue<T> _myQueue;
+        private  Object _myQueueLockObj = new object();
+        private object _changeLocker = new object();
+
+        protected Queue<T> MyQueue
+        {
+            get
+            {
+                if (_myQueue == null)
+                {
+                    lock (_myQueueLockObj)
+                    {
+                        if (_myQueue == null)
+                        {
+                            _myQueue = new Queue<T>();
+
+                        }
+                    }
+                }
+                return _myQueue;
+            }
+        }
+
+        public async void Push(T obj)
+        {
+            await Task.Run(() =>
+            {
+                lock (_changeLocker)
+                {
+                    Console.WriteLine("Push {0}", obj);
+
+                    MyQueue.Enqueue(obj);
+
+                    Monitor.Pulse(_changeLocker);
+                }
+            });
+        }
+
+
+        public async Task<T> Pop()
+        {
+            T item = default(T);
+            await Task.Run(() =>
+            {
+                lock (_changeLocker)
+                {
+                    var i = 0;
+                    do
+                    {
+                        try
+                        {
+                            item = MyQueue.Dequeue();
+                            i++;
+                            Console.WriteLine("Pop {0}, {1}", item, MyQueue.Count);
+                        }
+                        catch (Exception ex)
+                        {
+                            Monitor.Wait(_changeLocker);
+                        }
+                    } while (i == 0);
+                }
+            });
+
+            return item;
+        }
+    }
+}
---
+++ kasp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("kasp2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kasp2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0240a48c-9e62-4f47-8c5a-b46e281f45d3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
